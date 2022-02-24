340fe7b6e974d57296770cdcdb5bef308d0e3722
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-24 13:35:13 -0800

    Add nodemon dependency

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.get('/test', (req, res) => {
+  const { name, fame } = req.query;
+
+  res.send(`Hellooooo ${name} with ${fame}`);
+});
+
+const port = 8888;
+
+app.listen(port, () => {
+  console.log(`Express running at port ${port}`);
+});
