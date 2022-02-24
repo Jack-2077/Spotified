c716f0167418b66df24a6905853b4026b537ec78
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-24 15:17:45 -0800

    Add query parameters to spotify auth

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const querystring = require('querystring');
 
 const app = express();
 
@@ -12,10 +13,32 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+const randomString = (length) =>
+  Math.random()
+    .toString(36)
+    .substr(2, length)
+    .split('')
+    .map((e) => (Math.random() < Math.random() ? e.toUpperCase() : e))
+    .join()
+    .replace(/,/g, '');
+
+const stateKey = 'spotify_auth_state';
+
 app.get('/login', (req, res) => {
-  res.redirect(
-    `https://accounts.spotify.com/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`
-  );
+  const state = randomString(16);
+  res.cookie(stateKey, state);
+
+  let scope = 'user-read-private user-read-email';
+
+  queryParams = querystring.stringify({
+    response_type: 'code',
+    client_id: CLIENT_ID,
+    redirect_uri: REDIRECT_URI,
+    scope: scope,
+    state,
+  });
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
 const port = 8888;
