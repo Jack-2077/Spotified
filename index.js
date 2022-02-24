d978fcd7115922c9cae36beb38b939c85c760cea
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-24 14:41:22 -0800

    Add redirection to spotify accounts page on login

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
+
+CLIENT_ID = process.env.CLIENT_ID;
+CLIENT_SECRET = process.env.CLIENT_SECRET;
+REDIRECT_URI = process.env.REDIRECT_URI;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.get('/test', (req, res) => {
-  const { name, fame } = req.query;
-
-  res.send(`Hellooooo ${name} with ${fame}`);
+app.get('/login', (req, res) => {
+  res.redirect(
+    `https://accounts.spotify.com/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}`
+  );
 });
 
 const port = 8888;
