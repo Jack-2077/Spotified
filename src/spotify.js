2f3b9b41aaecec5d31caae89a77be5b29fac717a
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-27 12:34:03 -0800

    Add keys for getting/refresing token in spotify.js from localstorage

--- src/spotify.js
+++ src/spotify.js
@@ -1,3 +1,19 @@
+// Map for localStorage keys
+const LOCALSTORAGE_KEYS = {
+  accessToken: 'spotify_access_token',
+  refreshToken: 'spotify_refresh_token',
+  expireTime: 'spotify_token_expire_time',
+  timestamp: 'spotify_token_timestamp',
+};
+
+// Map to retrieve localStorage values
+const LOCALSTORAGE_VALUES = {
+  accessToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.accessToken),
+  refreshToken: window.localStorage.getItem(LOCALSTORAGE_KEYS.refreshToken),
+  expireTime: window.localStorage.getItem(LOCALSTORAGE_KEYS.expireTime),
+  timestamp: window.localStorage.getItem(LOCALSTORAGE_KEYS.timestamp),
+};
+
 const getAccessToken = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const accessToken = urlParams.get('access_token');
