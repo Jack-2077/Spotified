55952dc6ec69e89782d4cf7f4f40544738c2e428
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-27 12:42:29 -0800

    Add function for refreshing token

--- src/spotify.js
+++ src/spotify.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 // Map for localStorage keys
 const LOCALSTORAGE_KEYS = {
   accessToken: 'spotify_access_token',
@@ -26,6 +28,42 @@ const hasTokenExpired = () => {
   }
   const millisecondsElapsed = Date.now() - Number(timestamp);
   return millisecondsElapsed / 1000 > Number(expireTime);
+};
+
+/**
+ * Use the refresh token in localStorage to hit the /refresh_token endpoint
+ * in our Node app, then update values in localStorage with data from response.
+ * @returns {void}
+ */
+const refreshToken = async () => {
+  try {
+    // Logout if there's no refresh token stored or we've managed to get into a reload infinite loop
+    if (
+      !LOCALSTORAGE_VALUES.refreshToken ||
+      LOCALSTORAGE_VALUES.refreshToken === 'undefined' ||
+      Date.now() - Number(LOCALSTORAGE_VALUES.timestamp) / 1000 < 1000
+    ) {
+      console.error('No refresh token available');
+      logout();
+    }
+
+    // Use `/refresh_token` endpoint from our Node app
+    const { data } = await axios.get(
+      `/refresh_token?refresh_token=${LOCALSTORAGE_VALUES.refreshToken}`
+    );
+
+    // Update localStorage values
+    window.localStorage.setItem(
+      LOCALSTORAGE_KEYS.accessToken,
+      data.access_token
+    );
+    window.localStorage.setItem(LOCALSTORAGE_KEYS.timestamp, Date.now());
+
+    // Reload the page for localStorage updates to be reflected
+    window.location.reload();
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 /**
