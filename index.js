b241fdcbd6656599a614803d2965eabd70464751
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-27 10:18:31 -0800

    Test refresh token in login route

--- index.js
+++ index.js
@@ -62,27 +62,44 @@ app.get('/callback', (req, res) => {
   })
     .then((response) => {
       if (response.status === 200) {
-        const { access_token, token_type } = response.data;
+        //     const { access_token, token_type } = response.data;
+
+        //     axios
+        //       .get('https://api.spotify.com/v1/me', {
+        //         headers: {
+        //           Authorization: `${token_type} ${access_token}`,
+        //         },
+        //       })
+        //       .then((response) =>
+        //         res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`)
+        //       );
+        //   } else {
+        //     res.send(response);
+        //   }
+        // })
+        // .catch((err) => {
+        //   res.send(err);
+        // });
 
         const { refresh_token } = response.data;
 
         axios
-          .get('https://api.spotify.com/v1/me', {
-            headers: {
-              Authorization: `${token_type} ${access_token}`,
-            },
+          .get(
+            `http://localhost:8888/refresh_token?refresh_token=${refresh_token}`
+          )
+          .then((response) => {
+            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
           })
-          .then((response) =>
-            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`)
-          );
+          .catch((error) => {
+            res.send(error);
+          });
       } else {
         res.send(response);
       }
     })
-    .catch((err) => {
-      res.send(err);
+    .catch((error) => {
+      res.send(error);
     });
-  // res.send('callback');
 });
 
 app.get('/refresh_token', (req, res) => {
