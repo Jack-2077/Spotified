e97fd35eafbc8260a3b626435c7baf7bcc0c23a1
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-27 12:36:20 -0800

    Add logic for retrieving the Spotify access token from localStorage

--- src/spotify.js
+++ src/spotify.js
@@ -14,6 +14,54 @@ const LOCALSTORAGE_VALUES = {
   timestamp: window.localStorage.getItem(LOCALSTORAGE_KEYS.timestamp),
 };
 
+/**
+ * Handles logic for retrieving the Spotify access token from localStorage
+ * or URL query params
+ * @returns {string} A Spotify access token
+ */
+const getAccessToken = () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const queryParams = {
+    [LOCALSTORAGE_KEYS.accessToken]: urlParams.get('access_token'),
+    [LOCALSTORAGE_KEYS.refreshToken]: urlParams.get('refresh_token'),
+    [LOCALSTORAGE_KEYS.expireTime]: urlParams.get('expires_in'),
+  };
+  const hasError = urlParams.get('error');
+
+  // If there's an error OR the token in localStorage has expired, refresh the token
+  if (
+    hasError ||
+    hasTokenExpired() ||
+    LOCALSTORAGE_VALUES.accessToken === 'undefined'
+  ) {
+    refreshToken();
+  }
+
+  // If there is a valid access token in localStorage, use that
+  if (
+    LOCALSTORAGE_VALUES.accessToken &&
+    LOCALSTORAGE_VALUES.accessToken !== 'undefined'
+  ) {
+    return LOCALSTORAGE_VALUES.accessToken;
+  }
+
+  // If there is a token in the URL query params, user is logging in for the first time
+  if (queryParams[LOCALSTORAGE_KEYS.accessToken]) {
+    // Store the query params in localStorage
+    for (const property in queryParams) {
+      window.localStorage.setItem(property, queryParams[property]);
+    }
+    // Set timestamp
+    window.localStorage.setItem(LOCALSTORAGE_KEYS.timestamp, Date.now());
+    // Return access token from query params
+    return queryParams[LOCALSTORAGE_KEYS.accessToken];
+  }
+
+  // We should never get here!
+  return false;
+};
+
 const getAccessToken = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const accessToken = urlParams.get('access_token');
