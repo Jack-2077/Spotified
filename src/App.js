88abba3d9b8c18f917edb00ce441c16ba7df367b
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-27 18:11:21 -0800

    Add axios get request for profile data and store the response in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { accessToken, logout } from './spotify';
+import { accessToken, logout, getCurrentUserProfile } from './spotify';
 
 function App() {
   const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
     setToken(accessToken);
+
+    const fetchData = async () => {
+      try {
+        const { data } = await getCurrentUserProfile();
+        setProfile(data);
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
