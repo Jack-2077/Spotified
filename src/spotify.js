ea0557255fc454605396674652ee7327faab0460
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-27 18:11:50 -0800

    Add axios get request for profile data

--- src/spotify.js
+++ src/spotify.js
@@ -136,3 +136,10 @@ export const accessToken = getAccessToken();
 axios.defaults.baseURL = 'https://api.spotify.com/v1';
 axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
 axios.defaults.headers['Content-Type'] = 'application/json';
+
+/**
+ * Get Current User's Profile
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-current-users-profile
+ * @returns {Promise}
+ */
+export const getCurrentUserProfile = () => axios.get('/me');
