bde0526a4c661ac3d67408f87210eab2353ed6ca
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-27 20:07:24 -0800

    Add ScrollToTop component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 
 import { Playlist, Playlists, TopArtists, TopTracks, Home } from './components';
+import ScrollToTop from './components/ScrollToTop';
 import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path='top-artists' element={<TopArtists />} />
-      <Route path='top-tracks' element={<TopTracks />} />
-      <Route path='playlists' element={<Playlists />}>
-        <Route path=':id' element={<Playlist />} />
-      </Route>
-    </Routes>
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='top-artists' element={<TopArtists />} />
+        <Route path='top-tracks' element={<TopTracks />} />
+        <Route path='playlists' element={<Playlists />}>
+          <Route path=':id' element={<Playlist />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
