18b4b1e9b146611ee71ff90118d205ba7fe014e7
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-27 18:04:16 -0800

    Add global axios defaults to spotify.js

--- src/spotify.js
+++ src/spotify.js
@@ -128,3 +128,11 @@ const getAccessToken = () => {
 };
 
 export const accessToken = getAccessToken();
+
+/**
+ * Axios global request headers
+ * https://github.com/axios/axios#global-axios-defaults
+ */
+axios.defaults.baseURL = 'https://api.spotify.com/v1';
+axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+axios.defaults.headers['Content-Type'] = 'application/json';
