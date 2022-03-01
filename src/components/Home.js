5e4e797616a3f94c764b04d8e65156b47c5199c4
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-02-28 17:50:08 -0800

    Add styled logout button

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,21 @@ import { Login, Profile } from './pages';
 import { accessToken, logout, getCurrentUserProfile } from '../spotify';
 import { catchErrors } from '../utils';
 
+import styled from 'styled-components/macro';
 import { GlobalStyle } from './styles';
+
+const StyledLoginButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: var(--green);
+  color: var(--white);
+  border-radius: var(--border-radius-pill);
+  font-weight: 700;
+  font-size: var(--fz-lg);
+  z-index: 10;
+`;
 
 function Home() {
   const [token, setToken] = useState(null);
@@ -31,7 +45,7 @@ function Home() {
         <Login />
       ) : (
         <>
-          <button onClick={logout}>Log out</button>
+          <StyledLoginButton onClick={logout}>Log out</StyledLoginButton>
           {profile && <Profile />}
         </>
       )}
