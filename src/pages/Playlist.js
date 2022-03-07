9a89802a09e1fa99166a8a52137f2ac7bd880327
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-03-06 19:16:32 -0800

    add tempo into sort options

--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -13,7 +13,7 @@ const Playlist = () => {
   const [tracks, setTracks] = useState(null);
   const [audioFeatures, setAudioFeatures] = useState(null);
   const [sortValue, setSortValue] = useState('');
-  const sortOptions = ['danceability', 'tempo', 'energy'];
+  const sortOptions = ['danceability', 'tempo', 'energy', 'tempo'];
 
   // Get playlist data based on ID from route params
   useEffect(() => {
