d5e7c8de0f3ea340066112d48fb81226e8a562e6
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-03-06 16:56:37 -0800

    feat: Add Playlist page

--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { catchErrors } from '../utils';
+import { getPlaylistById } from '../spotify';
+import { StyledHeader } from '../components/styles';
 
-function Playlist() {
-  return <div>Playlist</div>;
-}
+const Playlist = () => {
+  const { id } = useParams();
+  const [playlist, setPlaylist] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await getPlaylistById(id);
+      setPlaylist(data);
+    };
+
+    catchErrors(fetchData());
+  }, [id]);
+
+  return (
+    <>
+      {playlist && (
+        <>
+          <StyledHeader>
+            <div className='header__inner'>
+              {playlist.images.length && playlist.images[0].url && (
+                <img
+                  className='header__img'
+                  src={playlist.images[0].url}
+                  alt='Playlist Artwork'
+                />
+              )}
+              <div>
+                <div className='header__overline'>Playlist</div>
+                <h1 className='header__name'>{playlist.name}</h1>
+                <p className='header__meta'>
+                  {playlist.followers.total ? (
+                    <span>
+                      {playlist.followers.total}{' '}
+                      {`follower${playlist.followers.total !== 1 ? 's' : ''}`}
+                    </span>
+                  ) : null}
+                  <span>
+                    {playlist.tracks.total}{' '}
+                    {`song${playlist.tracks.total !== 1 ? 's' : ''}`}
+                  </span>
+                </p>
+              </div>
+            </div>
+          </StyledHeader>
+        </>
+      )}
+    </>
+  );
+};
 
 export default Playlist;
